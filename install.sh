d26d2938631c959cfb3620c1cb1827d382bf5c4c
FHYunCai <fhyuncai@163.com>
2021-05-19 20:58:09 +0800

    last fix bug..

--- install.sh
+++ install.sh
@@ -17,10 +17,11 @@ cat > start.sh << EOF
 #!/bin/sh
 
 proxy_port=10032
-proxy_ip=$(ip addr | grep inet | grep -v inet6 | grep -v '127.0.0.1' | grep -v docker | awk '{print $2}' | awk -F "/" '{print $1}')
-echo 主机名：$local_ip
-echo 端口：$proxy_port
-./mitmdump -s ./fcm.py --ssl-insecure -p $proxy_port --no-http2 -q
+proxy_ip=\$(ip addr | grep inet | grep -v inet6 | grep -v '127.0.0.1' | grep -v docker | awk '{print \$2}' | awk -F "/" '{print \$1}')
+[ ! \$proxy_ip ] && proxy_ip=此设备局域网IP
+echo 主机名：\$proxy_ip
+echo 端口：\$proxy_port
+mitmdump -s ./fcm.py --ssl-insecure -p \$proxy_port --no-http2 -q
 EOF
 
 chmod +x start.sh
